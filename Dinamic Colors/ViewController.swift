9982b30c6c2d67aaeb0576ca4169e4357e4ce621
Boris <brsbgrd@gmail.com>
2020-05-25 02:45:32 -0400

    code is done

--- Dinamic Colors/ViewController.swift
+++ Dinamic Colors/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorDisplayView: UIView!
+    
+    @IBOutlet var redIndexLabel: UILabel!
+    @IBOutlet var greenIndexLabel: UILabel!
+    @IBOutlet var blueIndexLabel: UILabel!
+    
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redIndexLabel.text = String(Float(round(redSliderOutlet.value * 1000) / 100))
+        greenIndexLabel.text = String(Float(round(greenSliderOutlet.value * 1000) / 100))
+        blueIndexLabel.text = String(Float(round(blueSliderOutlet.value * 1000) / 100))
+        
+        colorDisplayView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
+                                                   green: CGFloat(greenSliderOutlet.value),
+                                                   blue: CGFloat(blueSliderOutlet.value),
+                                                   alpha: 1.00)
     }
+    
 
-
+    @IBAction func redSlider() {
+        
+        redIndexLabel.text = String(Float(round(redSliderOutlet.value * 1000) / 100))
+        
+        colorDisplayView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
+                                                   green: CGFloat(greenSliderOutlet.value),
+                                                   blue: CGFloat(blueSliderOutlet.value),
+                                                   alpha: 1.00)
+    }
+    
+    @IBAction func greenSlider() {
+        
+        greenIndexLabel.text = String(Float(round(greenSliderOutlet.value * 1000) / 100))
+        
+        colorDisplayView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
+                                                   green: CGFloat(greenSliderOutlet.value),
+                                                   blue: CGFloat(blueSliderOutlet.value),
+                                                   alpha: 1.00)
+    }
+    
+    @IBAction func blueSlider() {
+       
+        blueIndexLabel.text = String(Float(round(blueSliderOutlet.value * 1000) / 100))
+        
+        colorDisplayView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
+                                                   green: CGFloat(greenSliderOutlet.value),
+                                                   blue: CGFloat(blueSliderOutlet.value),
+                                                   alpha: 1.00)
+    }
 }
 
